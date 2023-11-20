21853753404259167d40c0a4c50b485b2d741546
kkuchkarov <52906720+kkuchkarov@users.noreply.github.com>
2023-11-20 23:10:58 +0500

    finishhope

--- docs/js/authscript.js
+++ docs/js/authscript.js
@@ -1,33 +1,40 @@
 function login(username, password) {
+    var xhr = new XMLHttpRequest();
+    var url = 'http://test.aic.uz/apex/ins/swagger/auth/';
+
     // Формируем данные для запроса
-    const data = {
+    var data = JSON.stringify({
         p_username: username,
         p_password: password
-    };
+    });
 
-    // Опции запроса
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    };
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
 
-    // Отправка запроса на сервер
-    fetch('http://test.aic.uz/apex/ins/swagger/auth/', options)
-        .then(response => response.json())
-        .then(data => {
+    // Обработчик успешного выполнения запроса
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            var response = JSON.parse(xhr.responseText);
+
             // Обработка ответа от сервера
-            if (data.success) {
+            if (response.success) {
                 // Пользователь успешно аутентифицирован
                 window.location.href = "https://app.swaggerhub.com/apis-docs/apex.kobul/ords-generated_api_for_tengebank_autocredit/2.0.0#/";
             } else {
                 // Аутентификация не удалась
                 alert("Неверный логин или пароль");
             }
-        })
-        .catch(error => {
-            console.error('Ошибка:', error);
-        });
+        } else {
+            // Обработка ошибок при выполнении запроса
+            console.error('Ошибка запроса:', xhr.statusText);
+        }
+    };
+
+    // Обработчик ошибок
+    xhr.onerror = function () {
+        console.error('Ошибка сети');
+    };
+
+    // Отправка запроса на сервер
+    xhr.send(data);
 }
